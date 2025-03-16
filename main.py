a7df017960488c620646e0e7609815f3c683abb9
jakkritscpe <jakkrits.cpe@gmail.com>
2025-03-16 22:53:30 +0700

    will success version is now while loob check card.

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import customtkinter as ctk
 from tkinter import filedialog, messagebox
 from smartcard.System import readers
 
+SELECT = [0x00, 0xA4, 0x04, 0x00, 0x08]
+THAI_CARD = [0xA0, 0x00, 0x00, 0x00, 0x54, 0x48, 0x00, 0x01]
+COMMANDS = {
+    "CID": [0x80, 0xb0, 0x00, 0x04, 0x02, 0x00, 0x0d],
+    "TH Fullname": [0x80, 0xb0, 0x00, 0x11, 0x02, 0x00, 0x64],
+    "EN Fullname": [0x80, 0xb0, 0x00, 0x75, 0x02, 0x00, 0x64],
+    "DOB": [0x80, 0xb0, 0x00, 0xD9, 0x02, 0x00, 0x08],
+    "Gender": [0x80, 0xb0, 0x00, 0xE1, 0x02, 0x00, 0x01],
+    "Address": [0x80, 0xb0, 0x15, 0x79, 0x02, 0x00, 0x64]
+}
+
 def thai2unicode(data):
     return bytes(data).decode('tis-620').strip()
 
@@ -27,17 +38,6 @@ def read_id_card():
         connection = reader.createConnection()
         connection.connect()
         
-        SELECT = [0x00, 0xA4, 0x04, 0x00, 0x08]
-        THAI_CARD = [0xA0, 0x00, 0x00, 0x00, 0x54, 0x48, 0x00, 0x01]
-        COMMANDS = {
-            "CID": [0x80, 0xb0, 0x00, 0x04, 0x02, 0x00, 0x0d],
-            "TH Fullname": [0x80, 0xb0, 0x00, 0x11, 0x02, 0x00, 0x64],
-            "EN Fullname": [0x80, 0xb0, 0x00, 0x75, 0x02, 0x00, 0x64],
-            "DOB": [0x80, 0xb0, 0x00, 0xD9, 0x02, 0x00, 0x08],
-            "Gender": [0x80, 0xb0, 0x00, 0xE1, 0x02, 0x00, 0x01],
-            "Address": [0x80, 0xb0, 0x15, 0x79, 0x02, 0x00, 0x64]
-        }
-        
         connection.transmit(SELECT + THAI_CARD)
         
         data = {key: get_data(connection, cmd) for key, cmd in COMMANDS.items()}
@@ -49,11 +49,32 @@ def save_to_excel(file_path, sheet_name, data):
     try:
         wb = xw.Book(file_path)
         sheet = wb.sheets[sheet_name]
-        last_row = sheet.range("A" + str(sheet.cells.last_cell.row)).end('up').row + 1
-        sheet.range(f"A{last_row}").value = list(data.values())
-        wb.save()
-        messagebox.showinfo("Success", "Data saved successfully")
-        return "Data saved successfully"
+        last_row = sheet.range("A" + str(sheet.cells.last_cell.row)).end('up').row + 1    
+        
+        # get last row value -> []    
+        last_row_values = sheet.range(f"A{last_row - 1}").expand('right').value
+        
+        # map key value result.
+        card_data = dict(zip(list(COMMANDS.keys()), list(data.values())))
+        
+        # Read values from the selected column from row 1 to the last row
+        column_value = sheet.range(f"E1:E{last_row}").value
+        column_check = [str(value).replace(".0", "") for value in column_value]
+            
+        if not card_data["CID"] in column_check:
+            # create new order.
+            order = "A" + str(int(last_row_values[0].replace("A", "")) + 1)
+            new_data = [order, card_data["TH Fullname"], card_data["Address"],"-",card_data["CID"],""]
+            
+            sheet.range(f"A{last_row}").value = new_data
+            wb.save()
+            messagebox.showinfo("Success", "Data saved successfully")
+            
+        else:
+            messagebox.showwarning("Unscess", "This data already exists in the file.")
+            
+        return
+            
     except Exception as e:
         return str(e)
 
@@ -63,7 +84,12 @@ def select_file():
         file_var.set(file_path)
         wb = xw.Book(file_path)
         sheets = [sheet.name for sheet in wb.sheets]
-        sheet_var.set(sheets[0])
+        
+        if "Cus" in sheets:
+            sheet_var.set("Cus")
+        elif sheets:
+            sheet_var.set(sheets[0])
+        
         sheet_menu.configure(values=sheets)
 
 def process_card():
@@ -83,6 +109,7 @@ def process_card():
             
             save_to_excel(file_path, sheet_name, cleaned_data)
             # output_var.set(output_var.get() + "\n" + save_status)
+            
         elif error:
             output_var.set(error)
         else:
@@ -99,25 +126,26 @@ def update_output(*args):
 #######################
 #     UI Setup        #
 #######################
-ctk.set_appearance_mode("System")
+ctk.set_appearance_mode("Dark")
 root = ctk.CTk()
 root.title("ID Card Reader")
 root.geometry("300x300")
+root.resizable(False, False)
 
 file_var = ctk.StringVar()
 sheet_var = ctk.StringVar()
 output_var = ctk.StringVar()
 
 frame_file = ctk.CTkFrame(root)
-frame_file.pack(pady=10, padx=20, fill="x")
-label_file = ctk.CTkLabel(frame_file, text="Select Excel File:", anchor="w")
+frame_file.pack(pady=10, padx=20, fill="both")
+label_file = ctk.CTkLabel(frame_file, text="Select Excel File", anchor="w")
 label_file.pack(side="left", padx=10)
 button_browse = ctk.CTkButton(frame_file, text="Browse", command=select_file, fg_color="#EEEEEE", text_color="#000000")
 button_browse.pack(side="right", padx=10)
 
 frame_sheet = ctk.CTkFrame(root)
-frame_sheet.pack(pady=10, padx=20, fill="x")
-label_sheet = ctk.CTkLabel(frame_sheet, text="Select Sheet:", anchor="w")
+frame_sheet.pack(pady=10, padx=20, fill="both")
+label_sheet = ctk.CTkLabel(frame_sheet, text="Select Sheet", anchor="w")
 label_sheet.pack(side="left", padx=10)
 sheet_menu = ctk.CTkOptionMenu(frame_sheet, values=[], variable=sheet_var, fg_color="#EEEEEE", text_color="#000000")
 sheet_menu.pack(side="right", padx=10)
